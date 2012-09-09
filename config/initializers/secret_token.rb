347702ab5d181f35a095c483829467907a02d012
Marcelo Ribeiro <ribeirodesenv@gmail.com>
2012-09-08 20:51:13 -0700

    initial rails push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudtableRails::Application.config.secret_token = '1beafb2d596c1308320aa0f6ccf0ff273093b15c74d37118907b7b1b3610a262232f8a19c6cc435ac31f56353fbff4c5f61555dfac97d7d60b9e464c6dddee13'
