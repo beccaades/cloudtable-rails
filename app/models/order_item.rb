7619de2ecde5d1016013099e9ff051b80ec2d68d
Rafael Iga <rafael.f.iga@gmail.com>
2012-09-09 00:47:10 -0700

    order item - menu item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,8 +1,7 @@
 class OrderItem < ActiveRecord::Base
-  attr_accessible :order_id, :quantity, :menu_item_id, :order, :menu_item
+  attr_accessible :order_id, :quantity, :menu_item_id, :order
 
   validates :menu_item, :order, :presence => :true
 
   belongs_to :order
-  belongs_to :menu_item
 end
--- db/migrate/20120909054010_create_menu_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateMenuItems < ActiveRecord::Migration
-  def change
-    create_table :menu_items do |t|
-      t.string :venue_id
-
-      t.timestamps
-    end
-  end
-end
